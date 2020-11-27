9e70d8770dba2f8f77b1435fbd5c378a154b26b5
meetreks <meetreks@gmail.com>
2020-11-27 00:11:24 +0000

    Om Sai Ram

--- src/main/java/ApiGatewayApplication.java
+++ src/main/java/ApiGatewayApplication.java
@@ -17,6 +17,7 @@ import com.example.model.gateway;
  * 3. Just added this to Test Webhook with Jenkins. Now in the Pipeline DSL. Om Sai Ram - Please help me.
  * 4. Trying SonarQube in the Pipeline
  * 5. Trying again another method.
+ * 6. This is a feature branch where my Developers will make the changes. I will only run clean compile & J-units in this branch.
  * **/
 @RestController
 @SpringBootApplication(scanBasePackages = {"com.example.controler"})
